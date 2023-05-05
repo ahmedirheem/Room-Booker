7232be09fd220eb8c2de32dc9ea42e0e207a3c4b
unknown <ahmedirheem2003@gmail.com>
2023-05-06 00:19:49 +0300

    Routers Handling

--- server/router.js
+++ server/router.js
@@ -1,9 +1,14 @@
 import express from "express";
+import addRoom from "./controllers/addRoom.js";
+import getAllRooms from "./controllers/getAllRooms.js";
+import bookRoom from "./controllers/bookRoom.js";
+import adminDashboard from "./controllers/adminDashboard.js";
 
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World')
-});
+router.get('/', adminDashboard);
+router.get('/admin', getAllRooms);
+router.post('/admin/add', addRoom);
+router.post('/book', bookRoom);
 
-export default router
+export default router;
