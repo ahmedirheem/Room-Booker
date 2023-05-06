4e23030db3c5295f63a62aaa633fb84713039ac1
unknown <ahmedirheem2003@gmail.com>
2023-05-07 00:51:54 +0300

    Fixing Database

--- server/app.js
+++ server/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import {} from 'dotenv/config';
+import { } from 'dotenv/config';
 import router from './router.js';
 import connection from './database/connection.js';
 
@@ -10,15 +10,14 @@ app.set('PORT', process.env.PORT || 3000)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.use(express.static(path.join(path.dirname(new URL(import.meta.url).pathname), '..', 'public')));
-app.use(express.static('/Users/pc/Desktop/Code-Academy/Projects/Room-Booker/public'))
+app.use(express.static('public'))
 app.use(router)
 
 connection()
   .then(() => {
     app.listen(app.get('PORT'), () => {
       console.log(`Server is Running on http://localhost:${app.get('PORT')}`);
-    })    
+    })
   })
   .catch((err) => console.log(err))
 
--- server/database/connection.js
+++ server/database/connection.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
-const connection = () => mongoose.connect('mongodb://0.0.0.0:27017/Room-Booker')
+const connection = () => mongoose.connect('mongodb://localhost:27017/Room-Booker')
 
 export default connection;
